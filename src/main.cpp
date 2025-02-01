28d0b0b2f2b8e370a07985825142f6c671dff086
maxcabrajac <max@cabrajac.com>
2025-01-31 21:05:09 -0300

    Add prevworkspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include "globals.hpp"
+#include <exception>
+#include <expected>
 #include <functional>
+#include <iostream>
+#include <optional>
+#include <stdexcept>
+#include <string>
 #include <sstream>
 
 #include <hyprland/src/helpers/Monitor.hpp>
+#include <unordered_map>
 
-void setActiveWorkspaceOnMonitor(std::string arg_string) {
+typedef std::expected<std::optional<std::string>, std::exception> DispatchRet;
+
+template <typename T>
+using Err = std::unexpected<T>;
+
+std::unordered_map<MONITORID, WORKSPACEID> last_workspace;
+
+DispatchRet setActiveWorkspaceOnMonitor(std::string arg_string) {
 	std::stringstream args(arg_string);
 
 	WORKSPACEID wsId;
@@ -12,18 +26,17 @@ void setActiveWorkspaceOnMonitor(std::string arg_string) {
 
 	args >> wsId;
 	if (args.fail()) {
-		throw std::invalid_argument("Could not read workspace id");
+		return Err(std::invalid_argument("Could not read workspace id"));
 	}
 
 	args >> mId;
 	if (args.fail()) {
-		throw std::invalid_argument("Could not read monitor id");
+		return Err(std::invalid_argument("Could not read monitor id"));
 	}
 
 	auto mHandle = g_pCompositor->getMonitorFromID(mId);
 	if (!mHandle) {
-		throw std::invalid_argument(std::format("No monitor with id: {}", mId));
-		return;
+		return Err(std::invalid_argument(std::format("No monitor with id: {}", mId)));
 	}
 
 	auto wsHandle = g_pCompositor->getWorkspaceByID(wsId);
@@ -31,8 +44,38 @@ void setActiveWorkspaceOnMonitor(std::string arg_string) {
 		wsHandle = g_pCompositor->createNewWorkspace(wsId, mId);
 	}
 
+	auto last = mHandle->activeWorkspaceID();
+
 	g_pCompositor->moveWorkspaceToMonitor(wsHandle, mHandle, true);
 	mHandle->changeWorkspace(wsHandle, false, true, true);
+
+	last_workspace.insert_or_assign(mId, last);
+
+	return std::nullopt;
+}
+
+DispatchRet getPrevWorkspaceOfMonitor(std::string arg_string) {
+	std::stringstream args(arg_string);
+
+	MONITORID mId;
+
+	args >> mId;
+	if (args.fail()) {
+		return Err(std::invalid_argument("Could not read monitor id"));
+	}
+
+	auto mHandle = g_pCompositor->getMonitorFromID(mId);
+	if (!mHandle) {
+		return Err(std::invalid_argument(std::format("No monitor with id: {}", mId)));
+	}
+
+	WORKSPACEID wsId = -1;
+
+	if (last_workspace.contains(mId)) {
+		wsId = last_workspace.at(mId);
+	}
+
+	return std::format("{}", wsId);
 }
 
 // Do NOT change this function.
@@ -41,14 +84,20 @@ APICALL EXPORT std::string PLUGIN_API_VERSION()
 	return HYPRLAND_API_VERSION;
 }
 
-void addDispatcher(const std::string s, const std::function<void(std::string)> f) {
+void addDispatcher(const std::string s, const std::function<DispatchRet(std::string)> f) {
 	HyprlandAPI::addDispatcherV2(PHANDLE, std::format("bttr:{}", s), [f](std::string s) {
-		try {
-			f(s);
-		} catch (std::exception e) {
+		auto result = f(s);
+		if (result.has_value()) {
+			auto to_print = *result;
+			if (to_print.has_value()) {
+				return SDispatchResult { .success = false, .error = to_print.value() };
+			} else {
+				return SDispatchResult { .success = true };
+			}
+		} else {
+			auto e = result.error();
 			return SDispatchResult { .success = false, .error = e.what() };
 		}
-		return SDispatchResult { .success = true };
 	});
 }
 
@@ -57,6 +106,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle)
 	PHANDLE = handle;
 
 	addDispatcher("workspacemonitor", setActiveWorkspaceOnMonitor);
+	addDispatcher("prevworkspace", getPrevWorkspaceOfMonitor);
 	return {BTTR_NAME, BTTR_DESC, BTTR_AUTHOR, BTTR_VERSION};
 }
 
